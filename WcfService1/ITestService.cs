ed14a1f17b33d1baab1e29c33c26924bf8038950
donals <donals@DT8851FB4A55DD>
2018-07-03 16:57:51 +0200

    Add project files.

---
+++ WcfService1/ITestService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITestService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract(Name ="DoSumsAsync")]
+        System.Threading.Tasks.Task<double> DoSumsAsync(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double bigNumber, double smallNumber);
+
+        [OperationContract]
+        double ShowMyTakeHome(double earnings, double taxrate);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfService1/TestService.svc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TestService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TestService.svc or TestService.svc.cs at the Solution Explorer and start debugging.
+    public class TestService : ITestService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public async Task<double> DoSumsAsync(double n1, double n2)
+        {
+            try
+            {
+                return await Task<double>.Factory.StartNew(() =>
+                     {
+                         return n1 + n2;
+                     });
+            }
+            catch (AggregateException aEx)
+            {
+
+                throw aEx;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+        public double ShowMyTakeHome(double earnings, double taxrate)
+        {
+            var divisor = taxrate / 100;
+            return earnings - (earnings * divisor);
+        }
+    }
+}
